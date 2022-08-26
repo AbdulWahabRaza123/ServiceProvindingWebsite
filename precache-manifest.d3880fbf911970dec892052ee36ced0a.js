e53f96b5af5b71cd07eb274c8465565e04f9f7bf
Abdul Wahab Raza <abdulwahabraza123@gmail.com>
2022-08-26 19:28:16 +0500

    Updates

---
+++ precache-manifest.d3880fbf911970dec892052ee36ced0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f08234911f9f203f0f3",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/css/main.740eb6ee.chunk.css"
+  },
+  {
+    "revision": "1f08234911f9f203f0f3",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/js/main.1f082349.chunk.js"
+  },
+  {
+    "revision": "cdce463f87e86d99497f",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/css/1.a3c4c613.chunk.css"
+  },
+  {
+    "revision": "cdce463f87e86d99497f",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/js/1.cdce463f.chunk.js"
+  },
+  {
+    "revision": "68b4813daad9150fdaf3",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/js/runtime~main.68b4813d.js"
+  },
+  {
+    "revision": "a325b082d9d93022360cb1ef6b08817f",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/media/coding.a325b082.svg"
+  },
+  {
+    "revision": "cbc6761e1e624bbe87dea19a751af1dc",
+    "url": "/AbdulWahabRaza123.github.io/Services/static/media/services.cbc6761e.jpg"
+  },
+  {
+    "revision": "e2a5b59ae8fe5d550ebb54723045cb58",
+    "url": "/AbdulWahabRaza123.github.io/Services/index.html"
+  }
+];
